22fc714127138afaf6e704dce865c5fe196d55fe
MohamedKerkeb <kerkeb.moha@gmail.com>
2021-12-15 16:45:44 +0100

    3- Routing, redirect widlcard not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
+import { PageDetailsComponent } from './pages/page-details/page-details.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+//Creation de routes 
+const routes: Routes = [
+  // on dit a angular quand tu arrive tu vas sur ma page home obligation de mettre pathMatch 
+  {path: '' , redirectTo: 'home', pathMatch: 'full'}, 
+  {path: 'home', component: PageAccueilComponent},
+  {path: 'details', component: PageDetailsComponent},
+  {path: '**',  component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
+import { PageDetailsComponent } from './pages/page-details/page-details.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    PageAccueilComponent,
+    PageDetailsComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
