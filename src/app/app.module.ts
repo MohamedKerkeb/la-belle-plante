1c6374433452653a75409046a89cedfcb1f358c8
MohamedKerkeb <kerkeb.moha@gmail.com>
2021-12-16 11:23:16 +0100

    mise ne place les categories ds app-filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
 import { PageDetailsComponent } from './pages/page-details/page-details.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FilterSideBarComponent } from './components/filter-side-bar/filter-side-bar.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     PageAccueilComponent,
     PageDetailsComponent,
     PageNotFoundComponent,
+    FilterSideBarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as _ from 'underscore';
+import {list_products} from '../../data'
+
 @Component({
   selector: 'app-page-accueil',
   templateUrl: './page-accueil.component.html',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageAccueilComponent implements OnInit {
 
+  categories: string[] = [];
+  listData = list_products
+
   constructor() { }
 
   ngOnInit(): void {
+  
+  this.categories =  _.pluck(list_products, 'product_breadcrumb_label')
+  // this.categories = [...new Set(this.categories)]
+  // this.categories = _.filter(this.categories, (item, index) => this.categories.indexOf(item) === index)
+  this.categories = _.uniq(this.categories);
+
+  //console.log(this.categories)
+
+  const listAllCategories = this.listData.map(product => product.product_breadcrumb_label);
+  const listUniqCategories = _.uniq(listAllCategories)
+  console.log(listUniqCategories)
+  
   }
 
 }
